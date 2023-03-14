10d618ba787f958a729531c016102f6d15987b89
N1Official <dao.laszlo03@gmail.com>
2023-03-14 13:52:13 +0100

    create

--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -1,6 +1,49 @@
 @extends('layouts.main')
 @section('content')
     <div class="container">
-        kriét
+        <h1 class="text-center">{{$title}}</h1>
+        <form>
+            <div class="mb-3">
+                <label for="name">Név</label>
+                <input type="text" class="form-control" id="name">
+            </div>
+            <div class="mb-3">
+                <label for="gender">Nem</label>
+                <input type="text" class="form-control" id="gender" placeholder="Male/Female">
+            </div>
+            <div class="mb-3">
+                <label for="job">Munka</label>
+                <input type="text" class="form-control" id="job">
+            </div>
+            <div class="mb-3">
+                <label for="wand">Varázspálca</label>
+                <input type="text" class="form-control" id="wand">
+            </div>
+            <div class="mb-3">
+                <label for="Patronus">Patronus</label>
+                <input type="text" class="form-control" id="Patronus">
+            </div>
+            <div class="mb-3">
+                <label for="species">Faj</label>
+                <input type="text" class="form-control" id="species">
+            </div>
+            <div class="mb-3">
+                <label for="birth">Született</label>
+                <input type="date" class="form-control" id="birth">
+            </div>
+            <div class="mb-3">
+                <label for="death">Meghalt</label>
+                <input type="date" class="form-control" id="death">
+            </div>
+            <div class="mb-3">
+                <label for="house">Example select</label>
+                <select class="form-select">
+                    <option value="Griffendél">Griffendél</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+            <button type="button" class="btn btn-success">Mentés!</button>
+        </form>
     </div>
 @endsection
